327948a7d7d7b6677ac773e7daf32eb4d98fd8db
allenluna <allenluna3799@gmail.com>
2023-10-14 10:20:44 +0800

    latest_secret_key

--- website/__init__.py
+++ website/__init__.py
@@ -12,7 +12,7 @@ db_path = os.path.realpath("website/database")
 def create_app():
 
     app = Flask(__name__)
-    # app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+    app.secret_key = os.environ.get("SECRET_KEY")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
